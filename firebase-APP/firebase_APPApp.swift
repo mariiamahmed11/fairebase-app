12d5962e8188556768d3cc93a7b24e8418f6966c
mariam <mariiamahmed11@hotmail.com>
2023-06-05 13:29:56 +0300

    Initial Commit

---
+++ firebase-APP/firebase_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebase_APPApp.swift
+//  firebase-APP
+//
+//  Created by user on 05/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct firebase_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
