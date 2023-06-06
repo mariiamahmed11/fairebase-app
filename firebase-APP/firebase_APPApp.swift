9a07fd2f38c965d1d2332ddafa4b3bc8cf014e27
mariam <mariiamahmed11@hotmail.com>
2023-06-06 11:35:57 +0300

    folder added

--- firebase-APP/firebase_APPApp.swift
+++ firebase-APP/firebase_APPApp.swift
@@ -6,12 +6,27 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import Foundation
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct firebase_APPApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
     }
+  }
 }
